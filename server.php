9cf6bf33301b2436be354eb46ee97b03f749a55d
nr-0909 <69627014+nr-0909@users.noreply.github.com>
2020-10-01 20:07:40 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-$db=mysqli_connect('localhost','root',"","student_details");
+$db=mysqli_connect('database-1.czponyvcdebt.us-east-1.rds.amazonaws.com','admin',"admin123","student_details");
 if(isset($_POST['reg_user']))
 {
 	$roll_number = mysqli_real_escape_string($db, $_POST['roll_number']);
